54e595345124b01f377b9e8da387092e7c34323c
adamb910 <sfttcltd@gmail.com>
2018-04-29 12:31:06 +0200

    player kirajzolas

--- src/views/WorkerView.java
+++ src/views/WorkerView.java
@@ -1,6 +1,8 @@
 package views;
 
 import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class WorkerView extends ViewBase{
 
@@ -9,4 +11,13 @@ public class WorkerView extends ViewBase{
 	
 	}
 	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.drawOval(super.x, super.y, mezomeret * 3/4, mezomeret * 3/4);		
+		g2d.drawOval(super.x, super.y, mezomeret * 1/2, mezomeret * 1/2);
+	}
 }
